097cfc49c74eb020154e3cef805fbb7690028b4b
pumak254 <cesar_chavira@att.net>
2016-02-01 01:30:45 +0000

    all Dessert tests passed

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,42 @@
 class Dessert
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
   def healthy?
-    # your code here
+    calories < 200
   end
   def delicious?
-    # your code here
+    true
+  end
+  def name 
+    @name
+  end
+  # setters & getters
+  def name=(new_name)
+    @name = new_name
+  end
+  def calories
+    @calories
+  end
+  def calories=(new_calories)
+    @calories = new_calories
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @name = flavor + " " + "jelly bean"
+    @calories = 5
+    @flavor = flavor
+  end
+  def flavor
+    @flavor
+  end
+  def flavor=(new_flavor)
+    @flavor = new_flavor
+  end
+  def delicious?
+    true unless flavor == "licorice"
   end
 end
