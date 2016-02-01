d3e164b9423ac198777cc96284741f3801495ad5
pumak254 <cesar_chavira@att.net>
2016-02-01 02:38:28 +0000

    all RockPaperScissors tests passed

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -4,11 +4,24 @@ class RockPaperScissors
   class NoSuchStrategyError < StandardError ; end
 
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+    p1 = player1.last 
+    p2 = player2.last
+    unless ['R','P','S'].include?(p1) && ['R','P','S'].include?(p2)
+      raise NoSuchStrategyError.new("Strategy must be one of R,P,S")  
+    end
+    if (p1 == 'R' && p2 == 'P') || (p1 == 'S' && p2 == 'R') || (p1 == 'P' && p2 == 'S')
+      return player2
+    else
+      return player1
+    end
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    if tournament.first.first.is_a?(String) # base case is arr of arr of str
+      return winner(tournament.first, tournament.last) # base case
+    else
+      winner(tournament_winner(tournament.first), tournament_winner(tournament.last))
+    end
   end
 
 end
