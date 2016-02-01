1753d3a8b1455e9f4e4655c169a69a127fa273f9
pumak254 <cesar_chavira@att.net>
2016-02-01 00:37:18 +0000

    All tests passed for Fun With Strings

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -5,7 +5,7 @@ module FunWithStrings
     return self == self.reverse
   end
   def count_words
-    count_hash = Hash.new(0)
+    count_hash = Hash.new(0) # zero count for all words
     self.gsub!(/[!-@]|[\[-`]|[\{-~]/,'') # remove punctuation
     words = self.split(' ')
     words.each do |word| 
@@ -15,7 +15,20 @@ module FunWithStrings
     return count_hash
   end
   def anagram_groups
-    # your code here
+    anagram_arrays = []
+    words = self.split(" ")
+    words.each do |word|
+      new_anagram = true
+      anagram_arrays.each do |array| # check if word belongs to current groups
+        if array.first.downcase.chars.sort == word.downcase.chars.sort
+          array.push(word) # word belongs to this group
+          new_anagram = false
+          break
+        end
+      end
+      anagram_arrays.push([word]) if new_anagram # new group of anagrams
+    end
+    return anagram_arrays
   end
 end
 
